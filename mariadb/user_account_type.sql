ee20be46fe6b9cce34d64465d5004ed7192ecf66
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-01-20 16:35:38 +0000

    Create user_account_type.sql
    A YAP reference table for the user type and an insert statement.

---
+++ mariadb/user_account_type.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `user_account_type`
+(
+  `id` INT NOT NULL,
+  `description` VARCHAR(100) NOT NULL,
+PRIMARY KEY(`id`)
+)
+ENGINE = InnoDB;
+
+INSERT INTO user_account_type (id, description)
+VALUES
+(100, 'A YAP admin account can create, read, update and delete all user accounts, groups and PBXs'),
+(101, 'A YAP regular account can read all user accounts, groups and PBXs'),
+(200, 'A group admin can read and update thier own PBX(s) and group'),
+(201, 'A group regular account can read thier own PBX(s) and group'),
+(300, 'A PBX admin account can read and update thier own PBX'),
+(301, 'A PBX regular account can read thier own PBX');
