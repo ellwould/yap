1ab7efcb4ce7890ff135e64e0291d510424cf6ab
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-01-22 15:34:59 +0000

    Update pbx.sql
    BIGINT UNSIGNED

--- mariadb/pbx.sql
+++ mariadb/pbx.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `pbx.sql`
 (
-  `id` INT NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `pbx_name` VARCHAR(75) NOT NULL,
-  `pbx_site_address_id` INT NOT NULL,
-  `pbx_invoice_address_id` INT NOT NULL,
-  `group_id` INT NOT NULL,
+  `pbx_site_address_id` BIGINT UNSIGNED NOT NULL,
+  `pbx_invoice_address_id` BIGINT UNSIGNED NOT NULL,
+  `group_id` BIGINT UNSIGNED NOT NULL,
   `date_added` DATETIME NOT NULL,
   `pbx_active` BOOLEAN NOT NULL,
 PRIMARY KEY(`id`)
