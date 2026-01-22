c2e93744ca6beb7f7766ef1b64384e2dd0537a0a
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-01-22 15:37:36 +0000

    Update user_account.sql
    BIGINT UNSIGNED

--- mariadb/user_account.sql
+++ mariadb/user_account.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `user_account`
 (
-  `id` INT NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `first_name` VARCHAR(100) NOT NULL,
   `last_name` VARCHAR(100) NOT NULL,
-  `user_account_type_id` INT NOT NULL,
-  `group_id` INT NOT NULL,
-  `pbx_id` INT NOT NULL,
+  `user_account_type_id` BIGINT UNSIGNED NOT NULL,
+  `group_id` BIGINT UNSIGNED NOT NULL,
+  `pbx_id` BIGINT UNSIGNED NOT NULL,
   `date_added` DATETIME NOT NULL,
   `account_active` BOOLEAN NOT NULL,
 PRIMARY KEY(`id`)
