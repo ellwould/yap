4532687f11a5ff05050403845c7848bd6de06ba4
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-01-24 15:46:00 +0000

    Update group.sql

--- mariadb/group.sql
+++ mariadb/group.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `group.sql`
 (
-  `id` INT NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `group_name` VARCHAR(100) NOT NULL,
-  `group_site_address_id` INT NOT NULL,
-  `group_invoice_address_id` INT NOT NULL,
+  `group_invoice_address_id` BIGINT UNSIGNED NOT NULL,
+  `group_site_address_id` BIGINT UNSIGNED NOT NULL,
   `date_added` DATETIME NOT NULL,
   `group_active` BOOLEAN NOT NULL,
   `note` VARCHAR(255),
