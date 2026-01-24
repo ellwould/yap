1433c539fafcc752a9de3374f7731a60e488268e
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-01-24 18:47:27 +0000

    Update foreign_key.sql
    Changed foreign key names.

--- mariadb/foreign_key.sql
+++ mariadb/foreign_key.sql
@@ -1,28 +1,28 @@
 ALTER TABLE `group_invoice_address`
-ADD CONSTRAINT fk_____group_invoice_address__id_____group__id
+ADD CONSTRAINT fk___group_invoice_address___group
 FOREIGN KEY (`id`)
 REFERENCES `group` (`id`)
 ON DELETE CASCADE;
 
 ALTER TABLE `group_site_address`
-ADD CONSTRAINT fk_____group_site_address__id_____group__id
+ADD CONSTRAINT fk___group_site_address___group
 FOREIGN KEY (`id`)
 REFERENCES `group` (`id`)
 ON DELETE CASCADE;
 
 ALTER TABLE `pbx_invoice_address`
-ADD CONSTRAINT fk_____pbx_invoice_address__id_____group__id
+ADD CONSTRAINT fk___pbx_invoice_address___group
 FOREIGN KEY (`id`)
 REFERENCES `group` (`id`)
 ON DELETE CASCADE;
 
 ALTER TABLE `pbx_site_address`
-ADD CONSTRAINT fk_____pbx_site_address__id_____group__id
+ADD CONSTRAINT fk___pbx_site_address___group
 FOREIGN KEY (`id`)
 REFERENCES `group` (`id`)
 ON DELETE CASCADE;
 
 ALTER TABLE `user_account`
-ADD CONSTRAINT fk_____user_account__user_account_type_id_____user_account_type__id
+ADD CONSTRAINT fk___user_account___user_account_type
 FOREIGN KEY (`id`)
 REFERENCES `user_account_type` (`id`)
