43dbeabe9cdd518627369438f2b39895dcf71309
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-06-18 13:07:15 +0100

    Update yap.sql
    Bug Fixes

--- mariadb/yap.sql
+++ mariadb/yap.sql
@@ -129,7 +129,9 @@ ENGINE = InnoDB;
 -- Add pbx_id and and endpoint_type column to Asterisk tables
 
 ALTER TABLE `ps_endpoints`
-ADD COLUMN `endpoint_type` ENUM ('sip_extension','sip_trunk','webrtc_extension') NOT NULL;
+ADD COLUMN `endpoint_type` ENUM ('sip_extension', 'webrtc_extension') NOT NULL;
+
+ALTER TABLE `ps_endpoints`
 ADD COLUMN `pbx_id` BIGINT UNSIGNED NOT NULL;
 
 ALTER TABLE `ps_aors`
