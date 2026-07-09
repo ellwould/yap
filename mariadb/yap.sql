21138fbc6f62083f2768e7ef51222a7734113cba
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-07-09 02:51:15 +0100

    Update yap.sql
    Made varchar bigger

--- mariadb/yap.sql
+++ mariadb/yap.sql
@@ -108,7 +108,7 @@ CREATE TABLE `user_account_type`
 (
   `id` SMALLINT UNSIGNED NOT NULL,
   `type` VARCHAR(255) NOT NULL,
-  `permission` VARCHAR(4500) NOT NULL,
+  `permission` VARCHAR(4600) NOT NULL,
 PRIMARY KEY(`id`)
 )
 ENGINE = InnoDB;
