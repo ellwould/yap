c631ca4f7db97ced31ddeb5f9631c4ec5a815bf5
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-08-07 07:51:58 +0100

    Update yap.sql
    Removed ⊛ CDR Importer Tag ⊛

--- mariadb/yap.sql
+++ mariadb/yap.sql
@@ -772,8 +772,7 @@ VALUES
   ('⊛ YAP PBX Cease ⊛', 'Services', '⊛ YAP (Yet Another PBX) ⊛', NULL),
   ('⊛ YAP Extension Setup ⊛', 'Services', '⊛ YAP (Yet Another PBX) ⊛', NULL),
   ('⊛ YAP Extension Rental ⊛', 'Services', '⊛ YAP (Yet Another PBX) ⊛', NULL),
-  ('⊛ YAP Extension Cease ⊛', 'Services', '⊛ YAP (Yet Another PBX) ⊛', NULL),
-  ('⊛ CDR Importer Tag ⊛', 'Services', '⊛ YAP (Yet Another PBX) ⊛', NULL);
+  ('⊛ YAP Extension Cease ⊛', 'Services', '⊛ YAP (Yet Another PBX) ⊛', NULL);
 
 INSERT INTO `customer` (`id`, `name`, `uk_based`, `consumer_type`, `uk_vat_registered`, `uk_vat_number`, `reselling_minutes`, `pbx_limit`, `pbx_sales_tax_rate`, `pbx_sales_tax_status`, `ext_sales_tax_rate`, `ext_sales_tax_status`, `pbx_setup_price`, `pbx_rental_price`, `pbx_cease_price`, `pbx_contract_length`, `ext_setup_price`, `ext_rental_price`, `ext_cease_price`, `ext_contract_length`)
 VALUES (1, 'system', 'n/a', 'n/a', 'n/a', NULL, 'n/a', 0, 0, 'n/a', 0, 'n/a', 0, 0, 0, NULL, 0, 0, 0, NULL);
